f8f7492a936e1f860a127b9f6beab46d488b6c46
joelmena <joelmena56@hotmail.com>
2021-08-08 23:21:34 -0400

    agregado JWT y Swagger al proyecto. Revisar el comentario anterior

---
+++ Core/Utilities/TokenGenerator.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace xmedicalehr.api.Core.Util
+{
+    public class TokenGenerator
+    {
+        private readonly IConfiguration _configuration;
+        public TokenGenerator(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string CreateToken(Account.User user, List<Claim> claims)
+        {
+            SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(_configuration.GetSection("Jwt:Key").Value));
+            SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            Microsoft.IdentityModel.Tokens.SecurityTokenDescriptor tokenDescriptor = new Microsoft.IdentityModel.Tokens.SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using System.Collections.Immutable;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,9 @@ using Microsoft.Extensions.Logging;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using xmedicalehr.api.Account;
 using xmedicalehr.api.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 namespace xmedicalehr.api
 {
@@ -30,9 +35,10 @@ namespace xmedicalehr.api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+            string connectionString = Configuration.GetConnectionString("default");
 
             services.AddControllers();
+            services.AddSignalR();
             services.AddDbContext<AppDbContext>(options =>
             {
                 options.UseMySql(connectionString, serverVersion: ServerVersion.AutoDetect(connectionString), x =>
@@ -46,8 +52,8 @@ namespace xmedicalehr.api
             {
                 options.SignIn.RequireConfirmedAccount = false;
             })
-                .AddEntityFrameworkStores<AppDbContext>()
-                .AddDefaultTokenProviders();
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
 
             services.Configure<IdentityOptions>(options =>
             {
@@ -69,11 +75,69 @@ namespace xmedicalehr.api
                     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
                 options.User.RequireUniqueEmail = false;
             });
+
+            services.AddAuthentication(opt => {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(opt => {
+                opt.RequireHttpsMetadata = true;
+                opt.SaveToken = true;
+                opt.TokenValidationParameters = new TokenValidationParameters {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(this.Configuration.GetSection("Jwt:Key").Value)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddCors(opt => {
+                opt.AddDefaultPolicy(builder => {
+                    builder.AllowAnyHeader();
+                    builder.AllowAnyMethod();
+                    builder.AllowAnyOrigin();
+                    builder.WithOrigins("http://localhost:5005")
+                        .AllowCredentials();
+                });
+            });
+
+            services.AddSwaggerGen(x =>
+			{
+				x.SwaggerDoc("v4", new OpenApiInfo
+				{
+					Version = "v4",
+					Title = "XMedicalEHR",
+					Description = "ASP.NET Core Web API para el sistema medico del Centro Medico Siglo 21",
+					TermsOfService = new Uri("https://example.com/terms"),
+					Contact = new OpenApiContact
+					{
+						Name = "NEXTCOM",
+						Email = string.Empty,
+						Url = new Uri("https://nxt-pro.com"),
+					},
+					License = new OpenApiLicense
+					{
+						Name = "Use under LICX",
+						Url = new Uri("https://example.com/license"),
+					}
+				});
+			});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger(x =>
+			{
+				x.SerializeAsV2 = true;
+			});
+
+			app.UseSwaggerUI(x =>
+			{
+				x.SwaggerEndpoint("/swagger/v4/swagger.json", "XMedicalEHR");
+			});
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
